ed87e60b0e6c61bd0f07629f52687322bc8d8179
makla12 <1234makla@gmail.com>
2025-03-03 13:27:31 +0100

    refactor into separete files

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Calculator app'),
+    );
+  }
+}
