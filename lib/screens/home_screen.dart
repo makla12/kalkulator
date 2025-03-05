347c180d708542997f829cce4cefde0acb6af195
makla12 <1234makla@gmail.com>
2025-03-05 09:22:09 +0100

    refactor for cleaner code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,26 +10,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final Map<String, double Function(double a, double b)> operators = {
-    "x":(a, b){return a * b;},
-    "/":(a, b){return a / b;},
-    "+":(a, b){return a + b;},
-    "-":(a, b){return a - b;},
-  };
-
-  final List<List<String>> operationOrder = [["x","/"], ["+","-"]];
+  final List<Map<String, double Function(double a, double b)>> _operations = [
+    {"x": (a, b) => a * b, "/": (a, b) => a / b},
+    {"+": (a, b) => a + b, "-": (a, b) => a - b},
+  ];
 
   String _operation = "";
   String _displayText = "";
 
-  void _updateDisplayText(){
+  void _updateDisplayText() {
     setState(() {
       _displayText = _operation.replaceAll(";", "");
     });
   }
 
   void _clear() {
-    if(_operation.isEmpty) return;
+    if (_operation.isEmpty) return;
 
     if (_operation[_operation.length - 1] == ";") {
       _operation = _operation.substring(0, _operation.length - 3);
@@ -40,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
     _updateDisplayText();
   }
 
-  void _clearAll(){
+  void _clearAll() {
     _operation = "";
 
     _updateDisplayText();
@@ -52,9 +48,13 @@ class _MyHomePageState extends State<MyHomePage> {
     _updateDisplayText();
   }
 
-  void _enterDecimalPoint(){
-    if((_operation.isEmpty ? false : _operation[_operation.length - 1] == ".")) return;
-    if(_operation.substring(_operation.contains(";") ? _operation.lastIndexOf(";") : 0).contains(".")) return;
+  void _enterDecimalPoint() {
+    if ((_operation.isEmpty ? false : _operation[_operation.length - 1] == "."))
+      return;
+    if (_operation
+        .substring(_operation.contains(";") ? _operation.lastIndexOf(";") : 0)
+        .contains("."))
+      return;
 
     _operation += ".";
 
@@ -62,13 +62,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _enterOperator(String operator) {
-    if(_operation.isEmpty || _operation[_operation.length - 1] == ";") _clear();
+    if (_operation.isEmpty || _operation[_operation.length - 1] == ";")
+      _clear();
     _operation += ";$operator;";
 
     _updateDisplayText();
   }
 
-  void _negative(){
+  void _negative() {
     if (_operation.contains(";") || _operation.isEmpty) return;
     if (_operation[0] == "-") {
       _operation = _operation.substring(1);
@@ -79,28 +80,29 @@ class _MyHomePageState extends State<MyHomePage> {
     _updateDisplayText();
   }
 
-  String _claculate(String operation){
-    if(operation.isEmpty || operation[operation.length - 1] == ";") return operation;
+  String _claculate(String operation) {
+    if (operation.isEmpty || operation[operation.length - 1] == ";") return operation;
     List<String> operationList = operation.split(";");
 
-    for(List<String> order in operationOrder){
-      for (int i = 0; i < operationList.length;) {
-        if (order.contains(operationList[i])) {
+    for (Map<String, double Function(double a, double b)> order in _operations) {
+      for (int i = 0; i < operationList.length; i++) {
+        if (order.keys.contains(operationList[i])) {
           String operator = operationList[i];
           double a = double.parse(operationList[i - 1]);
           double b = double.parse(operationList[i + 1]);
-          double newNumber = operators[operator]!(a, b);
+          double newNumber = order[operator]!(a, b);
           operationList.removeRange(i - 1, i + 2);
           i--;
           operationList.insert(i, newNumber.toString());
         }
-        i++;
       }
     }
-    
-    if(double.parse(operationList.first) % 1 == 0) return operationList.first.substring(0, operationList.first.length - 2);
 
-    return operationList.first;
+    String result = operationList.single;
+    
+    if (double.parse(result) % 1 == 0) return double.parse(result).toStringAsFixed(0);
+
+    return result;
   }
 
   @override
@@ -117,17 +119,75 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           Screen(text: _displayText),
 
-          CalculatorGrid(buttonsData: _buttonsData)
+          CalculatorGrid(buttonsData: _buttonsData),
         ],
       ),
     );
   }
 
   late final List<Map<String, void Function()>> _buttonsData = [
-    {"AC": _clearAll, "+/-" : _negative, "( )": () {}, "/": () {_enterOperator("/");}},
-    {"7": (){_enterDigit("7");}, "8": () {_enterDigit("8");}, "9": () {_enterDigit("9");}, "x": () {_enterOperator("x");}},
-    {"4": () {_enterDigit("4");}, "5": () {_enterDigit("5");}, "6": () {_enterDigit("6");}, "-": () {_enterOperator("-");}},
-    {"1": () {_enterDigit("1");}, "2": () {_enterDigit("2");}, "3": () {_enterDigit("3");}, "+": () {_enterOperator("+");}},
-    {"0": () {_enterDigit("0");}, ".": () {_enterDecimalPoint();}, "C" : _clear, "=": (){_operation = _claculate(_operation); _updateDisplayText();}},
+    {
+      "AC": _clearAll,
+      "+/-": _negative,
+      "( )": () {},
+      "/": () {
+        _enterOperator("/");
+      },
+    },
+    {
+      "7": () {
+        _enterDigit("7");
+      },
+      "8": () {
+        _enterDigit("8");
+      },
+      "9": () {
+        _enterDigit("9");
+      },
+      "x": () {
+        _enterOperator("x");
+      },
+    },
+    {
+      "4": () {
+        _enterDigit("4");
+      },
+      "5": () {
+        _enterDigit("5");
+      },
+      "6": () {
+        _enterDigit("6");
+      },
+      "-": () {
+        _enterOperator("-");
+      },
+    },
+    {
+      "1": () {
+        _enterDigit("1");
+      },
+      "2": () {
+        _enterDigit("2");
+      },
+      "3": () {
+        _enterDigit("3");
+      },
+      "+": () {
+        _enterOperator("+");
+      },
+    },
+    {
+      "0": () {
+        _enterDigit("0");
+      },
+      ".": () {
+        _enterDecimalPoint();
+      },
+      "C": _clear,
+      "=": () {
+        _operation = _claculate(_operation);
+        _updateDisplayText();
+      },
+    },
   ];
 }
